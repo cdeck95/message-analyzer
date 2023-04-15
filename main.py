b53dd886a46a621f63a1cce05ebc13033081cf91
Chris Deck <deckchris95@gmail.com>
2023-04-15 16:24:32 -0400

    starting code

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from py_imessage import imessage
+import pandas as pd
 
-phone_number = '1234567890'
-apple_id = 'your_apple_id'
-password = 'your_password'
+# Read the CSV file
+messages = pd.read_csv('./example_imessages.csv', parse_dates=['timestamp'])
 
-with imessage.apple_message_handler(phone_number, apple_id, password) as handler:
-    messages = handler.get_messages()
+# Display the first 5 rows of the DataFrame
+print(messages.head())
+
+# Perform any analysis, such as counting messages per sender
+sender_counts = messages['sender'].value_counts()
+print(sender_counts)
+
+# You can also analyze the content of messages, dates, and other information
+
+# Iterate over each message and print the contents
+for index, message in messages.iterrows():
+    print(f"Timestamp: {message['timestamp']}")
+    print(f"Sender: {message['sender']}")
+    print(f"Receiver: {message['receiver']}")
+    print(f"Text: {message['text']}")
+    print()  # Print an empty line for readability
